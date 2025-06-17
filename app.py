a50d679247ad7d926c02a7cca7c57af7a0ebfd30
Monishahee <164154392+Monishahee@users.noreply.github.com>
2025-06-17 17:43:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -187,5 +187,8 @@ def final_submit():
     except Exception as e:
         return f"❌ Firestore Error: {str(e)}", 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
